fdf275f6d5ba02d498dbe52749260bf6ec33cc01
joelvitorniino <joelprofessor13@gmail.com>
2023-02-27 19:14:54 -0300

    feat: part one

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="flex justify-center items-center">
+      <div className="flex justify-center items-center">
+        <h1 className="text-2xl font-bold ml-2">
+          <span className="ml-6">Sign In</span>
+        </h1>
+      </div>
+
+      <input type="text" name="username" placeholder="username" className="px-4 py-2 border rounded-md capitalize relative top-24 block" />
+    </div>
   )
 }
